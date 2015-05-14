78f1018c06d78db19c1201752222edfa75c68a46
Marcus Gonçalves <marquinhusgoncalves@hotmail.com>
2015-05-14 12:11:58 -0300

    Retirada de arquivos

---
+++ reginaalvesnutri/category-noticias.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+    <div class="titulo" id="noticias">NOTÍCIAS</div>
+        <ol class="breadcrumb">
+            <li class="active">Notícias</li>
+            <li><a href="index.php?cat=2">Artigos</a></li>
+            <li><a href="index.php?cat=3">Dicas</a></li>
+        </ol>
+    </div>
+
+    <div class="container">
+        <div class="col-md-8 col-sm-12 linha">
+            <div class="blog-post">
+                <?php if(have_posts()) :while (have_posts()) : the_post(); ?>
+                
+                    <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <span><?php the_time('j \d\e F \d\e Y'); ?></span>
+                    <div style="max-width: 100%; height: 30px"></div>
+                    
+                    <p class="blog-post-meta"><?php the_excerpt(); ?></p>
+            
+                <?php endwhile;
+
+                else :
+                    echo '<p> Nenhum artigo encontrado </p>';
+
+                endif; ?>
+
+            </div>
+        </div>
+        <?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
