69e97f9380f47649fa45acd9c0621f328829cd58
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2015-05-29 17:10:36 -0300

    Atualização

--- reginaalvesnutri/header.php
+++ reginaalvesnutri/header.php
@@ -1,61 +1,64 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta name="viewport" content="width=device-width">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,700|Noticia+Text:400,700" rel="stylesheet" type="text/css">
-		<title><?php bloginfo('name'); ?></title>
-		<?php wp_head(); ?>
-		<script type="text/javascript">jQuery(function($) {$("li#menu-item-59").children("a").attr('href', "#");});</script>
-		<link rel="icon" href="<?php bloginfo('stylesheet_directory');?>/img/favicon.ico">
-		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>	
-	    <script type="text/javascript">
-	
-	        function initialize() {
-	            var myLatlng = new google.maps.LatLng(-23.606657, -46.652769);
-	            var mapOptions = {
-	                zoom: 18,
-	                center: myLatlng,
-	                disableDefaultUI: false,
-	                scrollwheel: false,
-	                draggable: false
-	            }
-	            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	
-	            var marker = new google.maps.Marker({
-	                position: myLatlng,
-	                map: map,
-	                title: 'Regina Alves Nutricionista'
-	            });
-	
-	            var styles = [
-	                {
-	                    stylers: [
-	                        { hue: "#6C1F1F" },
-	                        { saturation: -20 }
-	                    ]
-	                },{
-	                    featureType: "road",
-	                    elementType: "geometry",
-	                    stylers: [
-	                        { lightness: 100 },
-	                        { visibility: "simplified" }
-	                    ]
-	                },{
-	                    featureType: "road.local",
-	                    elementType: "labels",
-	                }
-	            ];
-	
-	            map.setOptions({styles: styles});
-	        }
-	
-	        google.maps.event.addDomListener(window, 'load', initialize);
-	
-	    </script>
+<head>
+	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+	<meta name="author" content=“Marquinhus Goncalves“>
+	<meta name="description" content=“Regina Alves Nutricionista tudo sobre nutricao”>
+	<meta name="keywords" content=“Nutricao, alimentacao, personal diet, controle de peso“>
+	<meta name="robots" content="index, follow">
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width initial-scale=1.0">
+	<link rel="icon" href="<?php bloginfo('stylesheet_directory');?>/img/favicon.ico">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript">jQuery(function($) {$("li#menu-item-59").children("a").attr('href', "#");});</script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script> 
+	<script type="text/javascript">
+		
+		function initialize() {
+			var myLatlng = new google.maps.LatLng(-23.606657, -46.652769);
+			var mapOptions = {
+				zoom: 18,
+				center: myLatlng,
+				disableDefaultUI: false,
+				scrollwheel: false,
+				draggable: false
+			}
+			var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map,
+				title: 'Regina Alves Nutricionista'
+			});
+			
+			var styles = [
+			{
+				stylers: [
+				{ hue: "#6C1F1F" },
+				{ saturation: -20 }
+				]
+			},{
+				featureType: "road",
+				elementType: "geometry",
+				stylers: [
+				{ lightness: 100 },
+				{ visibility: "simplified" }
+				]
+			},{
+				featureType: "road.local",
+				elementType: "labels",
+			}
+			];
 
-	</head>
+			map.setOptions({styles: styles});
+		}
+		
+		google.maps.event.addDomListener(window, 'load', initialize);
+		
+	</script>
+	<?php wp_head(); ?>
+</head>
 
 <body>
 	<div id="stage">
@@ -70,18 +73,18 @@
 
 			<nav class = "nav navbar-default" role = "navigation" >
 				<div class = "container">
-				<!-- Brand and toggle get grouped for better mobile display --> 
-				  <div class = "navbar-header"> 
-				    <button type = "button" class = "navbar-toggle" data-toggle = "collapse" data-target = ".navbar-ex-collapse"> 
-				      <span class = "sr-only">Toggle navigation</span> 
-				      <span class = "icon-bar"></span> 
-				      <span class = "icon-bar"></span> 
-				      <span class = "icon-bar"></span> 
-				    </button> 
+					<!-- Brand and toggle get grouped for better mobile display --> 
+					<div class = "navbar-header"> 
+						<button type = "button" class = "navbar-toggle" data-toggle = "collapse" data-target = ".navbar-ex-collapse"> 
+						  <span class = "sr-only">Toggle navigation</span> 
+						  <span class = "icon-bar"></span> 
+						  <span class = "icon-bar"></span> 
+						  <span class = "icon-bar"></span> 
+					  </button> 
 				  </div> 
 				  <!-- Collect the nav links, forms, and other content for toggling --> 
 				  <div class = "collapse navbar-collapse navbar-right navbar-ex-collapse">
-				    <?php /* Primary navigation */
+					<?php /* Primary navigation */
 					wp_nav_menu( array(
 					  'menu' => 'top_menu',
 					  'depth' => 2,
@@ -91,10 +94,10 @@
 					  'walker' => new wp_bootstrap_navwalker())
 					);
 					?>
-				  </div>
 				</div>
-			</nav>
+			</div>
+		</nav>
 
-		</header>
+	</header>
 
-		<div class="container">
+	<div class="container">
